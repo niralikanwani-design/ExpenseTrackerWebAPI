50ed1f354a45cf316032543686715cc6e96c3c7e
yash2376 <yash.variya@tatvasoft.com>
2025-11-20 16:00:00 +0530

    Dashboard changes

---
+++ ET.Domain/IRepository/IDashboardService.cs
@@ -0,0 +1,14 @@
+﻿using ET.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET.Domain.IRepository
+{
+    public interface IDashboardService
+    {
+        Task<DashboardModel> GetDashboardData(int userId, int month);
+    }
+}
